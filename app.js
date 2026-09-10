107079e616a9a96d6fa99e702f148aa0302ccd33
holinessfoundationsermons-sudo <holinessfoundationsermons@gmail.com>
2026-09-10 15:40:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ import {
   Firebase Console -> Project settings -> Your apps -> Web app.
 */
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_STORAGE_BUCKET",
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyAAiOuRXLfjcQRV4rA6twknG4ths7K07J4",
+  authDomain: "church-directory-a3793.firebaseapp.com",
+  projectId: "church-directory-a3793",
+  storageBucket: "church-directory-a3793.firebasestorage.app",
+  messagingSenderId: "19161243529",
+  appId: "1:19161243529:web:2e238ef54b2e0bfedecb58"
+};
 };
 
 const app = initializeApp(firebaseConfig);
